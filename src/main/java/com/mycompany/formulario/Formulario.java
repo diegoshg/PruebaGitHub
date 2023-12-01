170164fa09732455c20ea50a457a7bdd0f9c9f20
Alumno <Alumno@10.163.226.139>
2023-12-01 20:07:41 +0100

    Creacion del proyecto

--- src/main/java/com/mycompany/formulario/Formulario.java
+++ src/main/java/com/mycompany/formulario/Formulario.java
@@ -36,11 +36,11 @@ public class Formulario extends javax.swing.JFrame {
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 1112, Short.MAX_VALUE)
+            .addGap(0, 1012, Short.MAX_VALUE)
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 544, Short.MAX_VALUE)
+            .addGap(0, 496, Short.MAX_VALUE)
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
